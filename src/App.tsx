b4e11caccf94bc623b3d3f7bf9ccf70317006b15
Lena balakumar Subbarayan <lenabalakumar@gmail.com>
2022-03-18 12:11:20 +0530

    Second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,45 @@
+import axios from "axios";
+import React from "react";
+
 const App = () => {
-  return <div>Hi from Netlify</div>;
+  const [users, setUsers] = React.useState<any[]>([]);
+
+  const getData = () => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then((respose) => {
+        const data = respose.data;
+        setUsers(data);
+      })
+      .catch((e) => {
+        console.log("Error" + e);
+      });
+  };
+
+  React.useEffect(() => {
+    getData();
+  }, [users]);
+
+  return (
+    <div>
+      <table>
+        <tr>
+          <th>ID</th>
+          <th>Title</th>
+          <th>Body</th>
+        </tr>
+        {users.map((user, index) => {
+          return (
+            <tr key={index}>
+              <td>{user.id}</td>
+              <td>{user.title}</td>
+              <td>{user.body}</td>
+            </tr>
+          );
+        })}
+      </table>
+    </div>
+  );
 };
 
 export default App;
